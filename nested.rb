7534cac4fe2f96040cb69f92b104a8c829965a18
Amritpal Singh <singhamritpal49@gmail.com>
2019-06-11 04:46:03 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -1,15 +1,22 @@
 
 def hopper
 	programmer_hash =
-
-		{     :grace_hopper => {:known_for => "COBOL",:languages => ["COBOL", "FORTRAN"]},
-          :alan_kay => {:known_for => "Object Orientation", :languages => ["Smalltalk", "LISP"] },
-        :dennis_ritchie => {:known_for => "Unix", :languages => ["C"] }
+ 		{
+        :grace_hopper => {
+          :known_for => "COBOL",
+          :languages => ["COBOL", "FORTRAN"]
+        },
+        :alan_kay => {
+          :known_for => "Object Orientation",
+          :languages => ["Smalltalk", "LISP"]
+        },
+        :dennis_ritchie => {
+          :known_for => "Unix",
+          :languages => ["C"]
+        }
      }
 
 programmer_hash[:grace_hopper]
-#programmer_hash[:grace_hopper][:known_for]
-
 
 
 end
