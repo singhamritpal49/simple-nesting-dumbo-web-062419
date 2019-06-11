107ce64c15cf1cc33ba40d814d11917a2c431214
Amritpal Singh <singhamritpal49@gmail.com>
2019-06-11 04:33:50 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -16,12 +16,12 @@ def hopper
         }
      }
 
-
+return programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
-	programmer_hash[:alan_key]
+
 	programmer_hash =
  		{
         :grace_hopper => {
